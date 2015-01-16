f8ad57a3b37078cf5b490fbc8a5b72e754c3b756
Kot <o0kot0o@gmail.com>
2015-01-16 08:35:58 -0600

    Made the player walkable - TODO: Create player class Added double buffering. Created a 'Keyboard' class (Yes I know I'm using global variables!)

--- main.py
+++ main.py
@@ -7,12 +7,56 @@ SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
 
+# KEYS
+KEYLIST = [[False] for i in range(255)]
+
+playerx = SCREEN_WIDTH  / 2
+playery = SCREEN_HEIGHT / 2
+
 libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'Roguelike', False)
+con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+
 libtcod.sys_set_fps(LIMIT_FPS)
 
-while not libtcod.console_is_window_closed():
-    libtcod.console_set_default_foreground(0, libtcod.white)
-    libtcod.console_put_char(0, 1, 1, '@', libtcod.BKGND_NONE)
+class Keyboard:
+    global KEYLIST
 
+    def update(self):
+        key = libtcod.console_check_for_keypress(libtcod.KEY_PRESSED | libtcod.KEY_RELEASED)
+        if key.vk != libtcod.KEY_NONE:
+            if key.pressed == True:
+                print(key.c)
+                KEYLIST[key.c] = True
+            elif key.pressed == False:
+                KEYLIST[key.c] = False
+
+
+def handle_keys():
+    global playerx, playery
+
+kb = Keyboard()
+
+while not libtcod.console_is_window_closed():
+    libtcod.console_set_default_foreground(con, libtcod.white)
+    libtcod.console_put_char(con, playerx, playery, '@', libtcod.BKGND_NONE)
+
+
+    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
     libtcod.console_flush()
+
+    kb.update()
+
+    libtcod.console_put_char(con, playerx, playery, ' ', libtcod.BKGND_NONE)
+
+    if KEYLIST[ord('w')] == True:
+        playery -= 1
+    elif KEYLIST[ord('s')] == True:
+        playery += 1
+    elif KEYLIST[ord('d')] == True:
+        playerx += 1
+    elif KEYLIST[ord('a')] == True:
+        playerx -= 1
+
+    libtcod.sys_set_fps(LIMIT_FPS)
+
